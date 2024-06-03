e82b2cfed79934ce458dad389930686339a50049
Olamide Temiloluwa Iselowo <37885555+OlamideTemi@users.noreply.github.com>
2024-06-03 08:00:15 +0100

    Create Day5Loops.py

---
+++ Codes/Day5Loops.py
@@ -0,0 +1,36 @@
+"""
+Task
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
+
+Example
+3
+
+The printout should look like this:
+
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n*i}")
+      
